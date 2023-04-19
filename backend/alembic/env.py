8a7327b008db7298d98505a0ae5a49fc384489fc
Omar Selo <omar.selo@canonical.com>
2023-04-19 13:16:40 +0300

    Remove unnecessary comments in alembic

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -5,12 +5,8 @@ from sqlalchemy import pool
 
 from alembic import context
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
@@ -19,11 +15,6 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 target_metadata = None
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 
 
 def run_migrations_offline() -> None:
