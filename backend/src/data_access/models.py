108e3ee40bc46b869018a626d276449e475587bf
nadzyah <nadzeya.hutsko@canonical.com>
2023-05-03 14:24:36 +0200

    Use Association classes for M2M. Make Family-Environment relationship M2M

--- backend/src/data_access/models.py
+++ backend/src/data_access/models.py
@@ -31,7 +31,7 @@ from sqlalchemy import (
     DateTime,
 )
 from sqlalchemy.sql import func, expression
-from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.dialects.postgresql import JSONB, ARRAY
 from sqlalchemy.orm import (
     DeclarativeBase,
     MappedAsDataclass,
@@ -77,22 +77,11 @@ class Stage(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(100), unique=True, index=True)
-    position: Mapped[int] = mapped_column(unique=True)
+    position: Mapped[int] = mapped_column(index=True)
     # Relationships
+    family_id = mapped_column(ForeignKey("family.id"))
     family: Mapped[Family] = relationship(back_populates="stages")
-    arterfacts: Mapped[List["Artefact"]] = relationship(back_populates="stage")
-
-
-# A table to represent the expected envoronments.
-# It's the M2M relationship between ArtefactGroup and Environment tables
-expected_environment = Table(
-    "expected_environment",
-    Base.metadata,
-    Column(
-        "artefact_group_id", ForeignKey("artefact_group.id"), primary_key=True
-    ),
-    Column("environment_id", ForeignKey("environment.id"), primary_key=True),
-)
+    artefacts: Mapped[List["Artefact"]] = relationship(back_populates="stage")
 
 
 class ArtefactGroup(Base):
@@ -107,35 +96,9 @@ class ArtefactGroup(Base):
     artefacts: Mapped[List["Artefact"]] = relationship(
         back_populates="artefact_group"
     )
-    environments: Mapped[List["Environment"]] = relationship(
-        secondary=expected_environment, back_populates="artefact_groups"
+    environments: Mapped[List["ExpectedEnvironment"]] = relationship(
+        back_populates="artefact_group"
     )
-
-
-# A table to represent the result of test execution.
-# It's the M2M relationship between Artefact and Environment tables
-test_execution = Table(
-    "test_execution",
-    Base.metadata,
-    Column("artefact_id", ForeignKey("artefact.id"), primary_key=True),
-    Column("environment_id", ForeignKey("environment.id"), primary_key=True),
-    Column("jenkins_link", String(200), nullable=True),
-    Column("c3_link", String(200), nullable=True),
-    Column("update_at", DateTime, onupdate=func.now()),
-    Column(
-        "status",
-        Enum(
-            "Not Started",
-            "In Progress",
-            "Passed",
-            "Failed",
-            "Error",
-            "Not Tested",
-            name="test_status_enum",
-        ),
-        server_default="Not Started",
-    ),
-)
 
 
 class Artefact(Base):
@@ -152,13 +115,16 @@ class Artefact(Base):
         nullable=True,
     )
     # Relationships
+    family_id = mapped_column(ForeignKey("family.id"))
     family: Mapped[Family] = relationship(back_populates="artefacts")
+    stage_id = mapped_column(ForeignKey("stage.id"))
     stage: Mapped[Stage] = relationship(back_populates="artefacts")
+    artefact_group_id = mapped_column(ForeignKey("artefact_group.id"))
     artefact_group: Mapped[ArtefactGroup] = relationship(
         back_populates="artefacts"
     )
-    environments: Mapped[List["Environment"]] = relationship(
-        secondary=test_execution, back_populates="artefacts"
+    environments: Mapped[List["TestExecution"]] = relationship(
+        back_populates="artefact"
     )
     # Default fields
     is_archived: Mapped[bool] = mapped_column(
@@ -177,10 +143,86 @@ class Environment(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(200), unique=True, index=True)
     # Relationships
-    family: Mapped[Family] = relationship(back_populates="environments")
-    artefacts: Mapped[List["Artefact"]] = relationship(
-        secondary=test_execution, back_populates="environments"
+    families: Mapped[List["FamilyEnvironment"]] = relationship(
+        back_populates="environment"
     )
-    artefact_groups: Mapped[List["ArtefactGroup"]] = relationship(
-        secondary=expected_environment, back_populates="environments"
+    artefacts: Mapped[List["TestExecution"]] = relationship(
+        back_populates="environment"
     )
+    artefact_groups: Mapped[List["ExpectedEnvironment"]] = relationship(
+        back_populates="environment"
+    )
+
+
+class TestExecution(Base):
+    """
+    A table to represent the result of test execution.
+    It's the M2M relationship between Artefact and Environment tables
+    """
+
+    __tablename__ = "test_execution"
+
+    artefact_id: Mapped[int] = mapped_column(
+        ForeignKey("artefact.id"), primary_key=True
+    )
+    environment_id: Mapped[int] = mapped_column(
+        ForeignKey("environment.id"), primary_key=True
+    )
+    jenkins_link: Mapped[str] = mapped_column(String(200), nullable=True)
+    c3_link: Mapped[str] = mapped_column(String(200), nullable=True)
+    updated_at: Mapped[timestamp] = mapped_column(onupdate=func.now())
+    status: Mapped[str] = mapped_column(
+        Enum(
+            "Not Started",
+            "In Progress",
+            "Passed",
+            "Failed",
+            "Not Tested",
+            name="test_status_enum",
+        ),
+        server_default="Not Started",
+    )
+    artefact: Mapped["Artefact"] = relationship(back_populates="environments")
+    environment: Mapped["Environment"] = relationship(
+        back_populates="artefacts"
+    )
+
+
+class ExpectedEnvironment(Base):
+    """
+    A table to represent the expected envoronments.
+    It's the M2M relationship between ArtefactGroup and Environment tables
+    """
+
+    __tablename__ = "expected_environment"
+
+    artefact_group_id: Mapped[int] = mapped_column(
+        ForeignKey("artefact_group.id"), primary_key=True
+    )
+    environment_id: Mapped[int] = mapped_column(
+        ForeignKey("environment.id"), primary_key=True
+    )
+    artefact_group: Mapped["ArtefactGroup"] = relationship(
+        back_populates="environments"
+    )
+    environment: Mapped["Environment"] = relationship(
+        back_populates="artefact_groups"
+    )
+
+
+class FamilyEnvironment(Base):
+    """
+    A table to represent the M2M relationship between Family and
+    Environment tables
+    """
+
+    __tablename__ = "family_environment"
+
+    family_id: Mapped[int] = mapped_column(
+        ForeignKey("family.id"), primary_key=True
+    )
+    environment_id: Mapped[int] = mapped_column(
+        ForeignKey("environment.id"), primary_key=True
+    )
+    family: Mapped["Family"] = relationship(back_populates="environments")
+    environment: Mapped["Environment"] = relationship(back_populates="family")
