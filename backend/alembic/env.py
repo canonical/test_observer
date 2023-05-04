e988254606386a4f836afb281293c2cc209dfc83
Nadzya Hut <84857215+nadzyah@users.noreply.github.com>
2023-05-04 12:22:29 +0200

    Add v0 data models (#6)
    * Add base v0 models
    
    * s/Page/Family and s/Lane/Stage
    
    * Fix typoes
    
    * Add more tests execution statuses
    
    * Update backend/src/data_access/models.py
    
    Co-authored-by: Omar Selo <omar.selo@canonical.com>
    
    * Update backend/src/data_access/models.py
    
    Co-authored-by: Omar Selo <omar.selo@canonical.com>
    
    * Use Association classes for M2M. Make Family-Environment relationship M2M
    
    * Make artefact name non-unique
    
    * Add duedate for artefacts
    
    * Remove redundant relations according to the spec
    
    ---------
    
    Co-authored-by: Omar Selo <omar.selo@canonical.com>

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -12,9 +12,8 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+from src.data_access import Base
+target_metadata = Base.metadata
 
 
 def run_migrations_offline() -> None:
---
+++ backend/src/data_access/models.py
@@ -0,0 +1,204 @@
+# Copyright 2023 Canonical Ltd.
+# All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Written by:
+#        Nadzeya Hutsko <nadzeya.hutsko@canonical.com>
+
+
+from typing import List
+from datetime import datetime, date
+from typing_extensions import Annotated
+
+from sqlalchemy import (
+    Column,
+    Table,
+    ForeignKey,
+    Enum,
+    String,
+    Date,
+)
+from sqlalchemy.sql import func, expression
+from sqlalchemy.dialects.postgresql import JSONB, ARRAY
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    MappedAsDataclass,
+    Mapped,
+    mapped_column,
+    relationship,
+)
+
+
+# DateTime custom type for mapped classes
+timestamp = Annotated[
+    datetime,
+    mapped_column(server_default=func.CURRENT_TIMESTAMP()),
+]
+date = Annotated[
+    date,
+    mapped_column(nullable=True),
+]
+
+
+class Base(MappedAsDataclass, DeclarativeBase):
+    """Subclasses will be converted to dataclasses"""
+
+
+class Family(Base):
+    """A model to represent artefact family object"""
+
+    __tablename__ = "family"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), unique=True, index=True)
+    created_at: Mapped[timestamp]
+    # Relationships
+    stages: Mapped[List["Stage"]] = relationship(back_populates="family")
+
+
+class Stage(Base):
+    """A model to represent artefact stage in the promotion cycle"""
+
+    __tablename__ = "stage"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), unique=True, index=True)
+    position: Mapped[int] = mapped_column(index=True)
+    # Relationships
+    family_id = mapped_column(ForeignKey("family.id"))
+    family: Mapped[Family] = relationship(back_populates="stages")
+    artefacts: Mapped[List["Artefact"]] = relationship(back_populates="stage")
+
+
+class ArtefactGroup(Base):
+    """A model to represent groups of artefacts"""
+
+    __tablename__ = "artefact_group"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(200), unique=True, index=True)
+    version_pattern: Mapped[str] = mapped_column(String(100))
+    # Relationships
+    artefacts: Mapped[List["Artefact"]] = relationship(
+        back_populates="artefact_group"
+    )
+    environments: Mapped[List["ExpectedEnvironment"]] = relationship(
+        back_populates="artefact_group"
+    )
+
+
+class Artefact(Base):
+    """A model to represent artefacts (snaps, debs, images)"""
+
+    __tablename__ = "artefact"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(200), index=True)
+    source: Mapped[dict] = mapped_column(JSONB)
+    created_at: Mapped[timestamp]
+    due_date: Mapped[date]
+    status: Mapped[str] = mapped_column(
+        Enum("Approved", "Marked as Failed", name="artefact_status_enum"),
+        nullable=True,
+    )
+    # Relationships
+    stage_id = mapped_column(ForeignKey("stage.id"))
+    stage: Mapped[Stage] = relationship(back_populates="artefacts")
+    artefact_group_id = mapped_column(ForeignKey("artefact_group.id"))
+    artefact_group: Mapped[ArtefactGroup] = relationship(
+        back_populates="artefacts"
+    )
+    environments: Mapped[List["TestExecution"]] = relationship(
+        back_populates="artefact"
+    )
+    # Default fields
+    is_archived: Mapped[bool] = mapped_column(
+        server_default=expression.false()
+    )
+
+
+class Environment(Base):
+    """
+    A model to represent environment (usually physical devices but also
+    can be more like containers)
+    """
+
+    __tablename__ = "environment"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(200), unique=True, index=True)
+    # Relationships
+    artefacts: Mapped[List["TestExecution"]] = relationship(
+        back_populates="environment"
+    )
+    artefact_groups: Mapped[List["ExpectedEnvironment"]] = relationship(
+        back_populates="environment"
+    )
+
+
+class TestExecution(Base):
+    """
+    A table to represent the result of test execution.
+    It's the M2M relationship between Artefact and Environment tables
+    """
+
+    __tablename__ = "test_execution"
+
+    artefact_id: Mapped[int] = mapped_column(
+        ForeignKey("artefact.id"), primary_key=True
+    )
+    environment_id: Mapped[int] = mapped_column(
+        ForeignKey("environment.id"), primary_key=True
+    )
+    jenkins_link: Mapped[str] = mapped_column(String(200), nullable=True)
+    c3_link: Mapped[str] = mapped_column(String(200), nullable=True)
+    updated_at: Mapped[timestamp] = mapped_column(onupdate=func.now())
+    status: Mapped[str] = mapped_column(
+        Enum(
+            "Not Started",
+            "In Progress",
+            "Passed",
+            "Failed",
+            "Not Tested",
+            name="test_status_enum",
+        ),
+        server_default="Not Started",
+    )
+    artefact: Mapped["Artefact"] = relationship(back_populates="environments")
+    environment: Mapped["Environment"] = relationship(
+        back_populates="artefacts"
+    )
+
+
+class ExpectedEnvironment(Base):
+    """
+    A table to represent the expected envoronments.
+    It's the M2M relationship between ArtefactGroup and Environment tables
+    """
+
+    __tablename__ = "expected_environment"
+
+    artefact_group_id: Mapped[int] = mapped_column(
+        ForeignKey("artefact_group.id"), primary_key=True
+    )
+    environment_id: Mapped[int] = mapped_column(
+        ForeignKey("environment.id"), primary_key=True
+    )
+    artefact_group: Mapped["ArtefactGroup"] = relationship(
+        back_populates="environments"
+    )
+    environment: Mapped["Environment"] = relationship(
+        back_populates="artefact_groups"
+    )
