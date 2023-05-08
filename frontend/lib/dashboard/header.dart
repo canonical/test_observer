21727a0e4ecfde0038ae82487b049b6f96a9639f
Omar Selo <omar.selo@canonical.com>
2023-05-08 13:38:20 +0300

    Remove unused list button for now

--- frontend/lib/dashboard/header.dart
+++ frontend/lib/dashboard/header.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:yaru/yaru.dart';
 import 'package:yaru_icons/yaru_icons.dart';
-import 'package:yaru_widgets/widgets.dart';
 
 import '../models/family.dart';
 import '../spacing.dart';
@@ -51,7 +50,6 @@ class Header extends StatelessWidget {
               ),
             ],
           ),
-          const YaruIconButton(icon: Icon(YaruIcons.unordered_list, size: 32)),
         ],
       ),
     );
