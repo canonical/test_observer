4e70814c432cb798cba43c1ee7b86eecb5d3eab7
Omar Selo <omar.selo@canonical.com>
2023-05-29 10:30:02 +0300

    Small changes

--- backend/src/controllers/families/families.py
+++ backend/src/controllers/families/families.py
@@ -1,3 +1,15 @@
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Written by:
+#        Nadzeya Hutsko <nadzeya.hutsko@canonical.com>
+#        Omar Abou Selo <omar.selo@canonical.com>
+
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from src.data_access.models import Family
