1667f05363f7c60c4c8ea3589b34d012dff3648f
Matias Piipari <matias.piipari@gmail.com>
2023-05-31 00:06:53 +0300

    Drops the DB migration script which was really a placeholder.

--- backend/bin/migrate_db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 1. Bring application units to maintenance mode
-# 2. Update application image resource (api-image resource)
-# 3. Run migration action on one of the units
-# 4. Scale unit count back up
-# 5. Bring application units back to normal mode
