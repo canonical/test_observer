db4c5a3db6dc043337cca122e285e8b26791a503
nadzyah <nadzeya.hutsko@canonical.com>
2023-06-13 13:37:55 +0300

    Fix style issues

--- backend/migrations/versions/2023_06_12_0914-5f817bd3ce08_.py
+++ backend/migrations/versions/2023_06_12_0914-5f817bd3ce08_.py
@@ -6,18 +6,18 @@ Create Date: 2023-06-12 09:14:15.560032+00:00
 
 """
 from alembic import op
-import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '5f817bd3ce08'
-down_revision = '6a80dad01d24'
+revision = "5f817bd3ce08"
+down_revision = "6a80dad01d24"
 branch_labels = None
 depends_on = None
 
 
 def upgrade() -> None:
-    op.execute("""
+    op.execute(
+        """
         CREATE VIEW latest_artefacts_view AS
         SELECT a.*
         FROM artefact a
@@ -26,11 +26,13 @@ def upgrade() -> None:
             SELECT stage_id, name, source, MAX(created_at) AS max_created
             FROM artefact
             GROUP BY stage_id, name, source
-       ) AS sq ON a.stage_id = sq.stage_id AND a.name = sq.name AND a.source = sq.source AND a.created_at = sq.max_created
-    """)
-
-def downgrade() -> None:
-    op.execute(
-        "DROP VIEW latest_artefacts_view"
+       ) AS sq ON a.stage_id = sq.stage_id
+         AND a.name = sq.name
+         AND a.source = sq.source
+         AND a.created_at = sq.max_created
+    """
     )
 
+
+def downgrade() -> None:
+    op.execute("DROP VIEW latest_artefacts_view")
