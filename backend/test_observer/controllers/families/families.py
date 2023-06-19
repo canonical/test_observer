247e77cff37d52386c3b588e365e9eec0f03985a
nadzyah <nadzeya.hutsko@canonical.com>
2023-06-19 14:33:39 +0300

    Use groupby from itertools and check family name

--- backend/test_observer/controllers/families/families.py
+++ backend/test_observer/controllers/families/families.py
@@ -1,3 +1,6 @@
+# Copyright 2023 Canonical Ltd.
+# All rights reserved.
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -10,9 +13,14 @@
 #        Nadzeya Hutsko <nadzeya.hutsko@canonical.com>
 #        Omar Abou Selo <omar.selo@canonical.com>
 
+
+from itertools import groupby
+
+
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from test_observer.data_access.models import Family
+from test_observer.data_access.models_enums import FamilyName
 from test_observer.data_access.setup import get_db
 from test_observer.data_access.repository import get_artefacts_by_family_name
 
@@ -28,14 +36,13 @@ def read_family(family_name: str, db: Session = Depends(get_db)):
     if family is None:
         raise HTTPException(status_code=404, detail="Family not found")
 
-    latest_artefacts = get_artefacts_by_family_name(db, family_name)  # type: ignore
+    family_name_enum_value = FamilyName(family_name)
+    latest_artefacts = get_artefacts_by_family_name(db, family_name_enum_value)
 
-    stage_artefact_dict: dict = {stage.id: [] for stage in family.stages}
-    for artefact in latest_artefacts:
-        stage_artefact_dict[artefact.stage_id].append(artefact)
+    stage_artefact_dict = groupby(latest_artefacts, lambda art: art.stage)
 
-    for stage in family.stages:
-        stage.artefacts = stage_artefact_dict[stage.id]
+    for stage, artefacts in stage_artefact_dict:
+        stage.artefacts = list(artefacts)
 
     family.stages = sorted(family.stages, key=lambda x: x.position)
     return family
