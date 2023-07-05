0533f585f9e1e08a7e81e5b59db864c9013c031d
Omar Selo <omar.selo@canonical.com>
2023-07-05 16:44:25 +0300

    Remove unused imports

--- frontend/lib/ui/dashboard/dashboard_header.dart
+++ frontend/lib/ui/dashboard/dashboard_header.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:yaru/yaru.dart';
-import 'package:yaru_icons/yaru_icons.dart';
 
 import '../../models/test_execution.dart';
 import '../spacing.dart';
