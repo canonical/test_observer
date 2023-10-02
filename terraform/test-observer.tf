d27ba7d2d78249f0388d6fd503c47cfbe50ed080
nadzyah <nadzya.info@gmail.com>
2023-10-02 13:04:35 +0300

    Add default value for dev env

--- terraform/test-observer.tf
+++ terraform/test-observer.tf
@@ -29,6 +29,7 @@ locals {
   sentry_dsn_map = {
     production  = "https://dd931d36e0c24681aaeed6abd312c896@sentry.is.canonical.com//66"
     staging     = "https://84a48d05b2444e47a7fa176b577bf85a@sentry.is.canonical.com//68",
+    development = ""
   }
 }
 
