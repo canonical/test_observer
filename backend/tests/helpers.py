6c6277a6a3f927f5216cb74fa21ed985ad27abff
Omar Selo <omar.selo@canonical.com>
2023-10-20 07:21:08 +0000

    Pass correct source when creating a dummy artefact

--- backend/tests/helpers.py
+++ backend/tests/helpers.py
@@ -1,17 +1,25 @@
 from datetime import datetime
 
 from sqlalchemy.orm import Session
+
 from test_observer.data_access.models import Artefact, Stage
 
 
 def create_artefact(db_session: Session, stage_name: str, **kwargs) -> Artefact:
     """Create a dummy artefact"""
     stage = db_session.query(Stage).filter(Stage.name == stage_name).first()
+    source = kwargs.get("source")
+    if not source:
+        if stage_name in ("edge", "beta", "candidate", "stable"):
+            source = {"store": "ubuntu", "track": "latest"}
+        else:
+            source = {"pocket": "proposed", "series": "jammy"}
+
     artefact = Artefact(
         name=kwargs.get("name", ""),
         stage=stage,
         version=kwargs.get("version", "1.1.1"),
-        source=kwargs.get("source", {"store": "ubuntu"}),
+        source=source,
         created_at=kwargs.get("created_at", datetime.utcnow()),
     )
     db_session.add(artefact)
