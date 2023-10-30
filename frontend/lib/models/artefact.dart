d150b1565fe8b14edb03e2c2af4a8cc2e3e88214
Omar Selo <omar.selo@canonical.com>
2023-10-30 17:22:26 +0300

    Remove artefact source

--- frontend/lib/models/artefact.dart
+++ frontend/lib/models/artefact.dart
@@ -7,14 +7,27 @@ part 'artefact.g.dart';
 
 @freezed
 class Artefact with _$Artefact {
+  const Artefact._();
+
   const factory Artefact({
     required int id,
     required String name,
     required String version,
-    required Map<String, dynamic> source,
+    @Default(null) String? track,
+    @Default(null) String? store,
+    @Default(null) String? series,
+    @Default(null) String? repo,
     required StageName stage,
   }) = _Artefact;
 
   factory Artefact.fromJson(Map<String, Object?> json) =>
       _$ArtefactFromJson(json);
+
+  Map<String, String> get details => {
+        'version': version,
+        if (track != null) 'track': track!,
+        if (store != null) 'store': store!,
+        if (series != null) 'series': series!,
+        if (repo != null) 'repo': repo!,
+      };
 }
