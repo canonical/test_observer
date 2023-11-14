876642897e359e9f01b18fb4715abc5b888680d3
Andrej Velichkovski <andrej.velichkovski@gmail.com>
2023-11-14 13:25:51 +0100

    Add unit test

--- backend/tests/external_apis/test_c3.py
+++ backend/tests/external_apis/test_c3.py
@@ -2,6 +2,8 @@ import base64
 import re
 from typing import TypedDict
 
+from requests import Request
+
 import pytest
 from requests_mock import Mocker
 
@@ -43,6 +45,46 @@ def prepare_c3api(
     monkeypatch.setenv("C3_CLIENT_SECRET", client_secret)
     c3 = C3Api()
     return c3, bearer_token
+
+
+def test_authenticate_and_send_invalid_token(
+    requests_mock: Mocker, prepare_c3api: tuple[C3Api, str]
+):
+    c3, bearer_token = prepare_c3api
+
+    UNAUTHORIZED_RESPONSE_CODES = [401, 403]
+    for error_code in UNAUTHORIZED_RESPONSE_CODES:
+        requests_mock.get(
+            re.compile("^https://certification.canonical.com/api/v2/.*"),
+            request_headers={"Authorization": f"Bearer {bearer_token}"},
+            status_code=error_code,
+        )
+
+        test_request = Request(
+            method="GET",
+            url="https://certification.canonical.com/api/v2/submissions/status",
+        )
+
+        # Reset mock to clear the request history
+        requests_mock.reset_mock()
+
+        c3._authenticate_and_send(test_request)
+
+        # Verify three requests were called: unauthenticated request,
+        # authentication request, repeated request with the correct authentication details
+        assert len(requests_mock.request_history) == 3
+        # Verify the last request was to the intended URL
+        assert (
+            requests_mock.request_history[-1].method == "GET"
+            and requests_mock.request_history[-1].url
+            == "https://certification.canonical.com/api/v2/submissions/status"
+        )
+        # Verify the second to last request was an authentication API call to get the token
+        assert (
+            requests_mock.request_history[-2].method == "POST"
+            and requests_mock.request_history[-2].url
+            == "https://certification.canonical.com/oauth2/token/"
+        )
 
 
 def test_get_submissions_statuses(
