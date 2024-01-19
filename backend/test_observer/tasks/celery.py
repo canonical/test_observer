a19f6018569f6811805323deb034939874774fcd
Omar Selo <omar.selo@canonical.com>
2024-01-19 10:17:11 +0000

    Add celery and redis to charm/juju/terraform

--- backend/test_observer/tasks/celery.py
+++ backend/test_observer/tasks/celery.py
@@ -1,6 +1,10 @@
+from os import environ
 from celery import Celery
 
-app = Celery("tasks", broker="redis://test-observer-redis")
+DEVELOPMENT_BROKER_URL = "redis://test-observer-redis"
+broker_url = environ.get("CELERY_BROKER_URL", DEVELOPMENT_BROKER_URL)
+
+app = Celery("tasks", broker=broker_url)
 
 if __name__ == "__main__":
     app.start()
