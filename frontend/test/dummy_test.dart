cb5132887f6aa4ef4317311552d771590ea245ba
Omar Selo <omar.selo@canonical.com>
2024-01-29 15:47:16 +0300

    Add filters test (the first test case)

--- frontend/test/dummy_test.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  test('dummy test to not fail CI', () {
-    expect(1, 1);
-  });
-}
---
+++ frontend/test/providers/filters_test.dart
@@ -0,0 +1,43 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+import 'package:testcase_dashboard/models/artefact.dart';
+import 'package:testcase_dashboard/models/family_name.dart';
+import 'package:testcase_dashboard/providers/api.dart';
+import 'package:testcase_dashboard/providers/family_artefacts.dart';
+import 'package:testcase_dashboard/providers/filters.dart';
+import 'package:testcase_dashboard/repositories/api_repository.dart';
+
+import '../dummy_data.dart';
+import '../utilities.dart';
+
+void main() {
+  test('it collects options from fetched artefacts', () async {
+    final container = createContainer(
+      overrides: [apiProvider.overrideWith((ref) => ApiRepositoryMock())],
+    );
+    const family = FamilyName.snap;
+
+    // Wait on artefacts to load cause filtersProvider uses requireValue
+    await container.read(familyArtefactsProvider(family).future);
+
+    final filters = container.read(filtersProvider(family));
+
+    expect(filters[0].name, 'Assignee');
+    expect(
+      filters[0].options,
+      [(name: dummyArtefact.assignee?.name, value: false)],
+    );
+    expect(filters[1].name, 'Status');
+    expect(
+      filters[1].options,
+      [(name: dummyArtefact.status.name, value: false)],
+    );
+  });
+}
+
+class ApiRepositoryMock extends Mock implements ApiRepository {
+  @override
+  Future<Map<int, Artefact>> getFamilyArtefacts(FamilyName family) async {
+    return {dummyArtefact.id: dummyArtefact};
+  }
+}
