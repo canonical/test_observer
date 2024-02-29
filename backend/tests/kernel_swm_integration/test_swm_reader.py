c11fdac79c16fe3fcdfe165f3bd5f56c71698f01
Omar Selo <omar.selo@canonical.com>
2024-02-29 12:07:53 +0000

    Get stage names from db instead of hardcoding them

--- backend/tests/kernel_swm_integration/test_swm_reader.py
+++ backend/tests/kernel_swm_integration/test_swm_reader.py
@@ -1,11 +1,12 @@
 from datetime import date
 
 from requests_mock import Mocker
+from sqlalchemy.orm import Session
 
 from test_observer.kernel_swm_integration.swm_reader import get_artefacts_swm_info
 
 
-def test_get_artefacts_swm_info(requests_mock: Mocker):
+def test_get_artefacts_swm_info(requests_mock: Mocker, db_session: Session):
     bug_id = "2052085"
     artefact_id = 22996
     requests_mock.get(
@@ -27,7 +28,7 @@ def test_get_artefacts_swm_info(requests_mock: Mocker):
         },
     )
 
-    artefacts_swm_info = get_artefacts_swm_info()
+    artefacts_swm_info = get_artefacts_swm_info(db_session)
 
     assert artefacts_swm_info == {
         artefact_id: {
