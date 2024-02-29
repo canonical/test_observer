a0f48c14c1af936e37bcc20f8e014c7cb7c7d994
Omar Selo <omar.selo@canonical.com>
2024-02-29 10:23:56 +0000

    Add scheduled celery task that integrates with kernel swm

--- backend/tasks/celery.py
+++ backend/tasks/celery.py
@@ -1,11 +1,32 @@
+import logging
 from os import environ
 
 from celery import Celery
+
+from test_observer.data_access.setup import SessionLocal
+from test_observer.kernel_swm_integration.swm_integrator import (
+    update_artefacts_with_tracker_info,
+)
+from test_observer.kernel_swm_integration.swm_reader import get_artefacts_swm_info
 
 DEVELOPMENT_BROKER_URL = "redis://test-observer-redis"
 broker_url = environ.get("CELERY_BROKER_URL", DEVELOPMENT_BROKER_URL)
 
 app = Celery("tasks", broker=broker_url)
+
+logger = logging.getLogger(__name__)
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):  # noqa
+    sender.add_periodic_task(300, integrate_with_kernel_swm.s())
+
+
+@app.task
+def integrate_with_kernel_swm():
+    swm_info = get_artefacts_swm_info()
+    update_artefacts_with_tracker_info(SessionLocal(), swm_info)
+
 
 if __name__ == "__main__":
     app.start()
