ab008aaf84cfdbf8e53e92c59eff59b7c95737de
Omar Selo <omar.selo@canonical.com>
2024-04-01 13:10:04 +0000

    Sort test executions by environment name

--- backend/tests/controllers/artefacts/test_artefacts.py
+++ backend/tests/controllers/artefacts/test_artefacts.py
@@ -144,6 +144,53 @@ def test_get_artefact_builds(
     ]
 
 
+def test_get_artefact_builds_sorts_test_executions_by_environment_name(
+    test_client: TestClient, generator: DataGenerator
+):
+    a = generator.gen_artefact("beta")
+    ab = generator.gen_artefact_build(a)
+    e2 = generator.gen_environment("e2")
+    e1 = generator.gen_environment("e1")
+    te2 = generator.gen_test_execution(ab, e2)
+    te1 = generator.gen_test_execution(ab, e1)
+
+    assert test_client.get(f"/v1/artefacts/{a.id}/builds").json() == [
+        {
+            "id": ab.id,
+            "revision": ab.revision,
+            "architecture": ab.architecture,
+            "test_executions": [
+                {
+                    "id": te1.id,
+                    "ci_link": te1.ci_link,
+                    "c3_link": te1.c3_link,
+                    "status": te1.status.value,
+                    "environment": {
+                        "id": e1.id,
+                        "name": e1.name,
+                        "architecture": e1.architecture,
+                    },
+                    "review_decision": [],
+                    "review_comment": "",
+                },
+                {
+                    "id": te2.id,
+                    "ci_link": te2.ci_link,
+                    "c3_link": te2.c3_link,
+                    "status": te2.status.value,
+                    "environment": {
+                        "id": e2.id,
+                        "name": e2.name,
+                        "architecture": e2.architecture,
+                    },
+                    "review_decision": [],
+                    "review_comment": "",
+                },
+            ],
+        }
+    ]
+
+
 def test_get_artefact_builds_only_latest(
     test_client: TestClient, generator: DataGenerator
 ):
