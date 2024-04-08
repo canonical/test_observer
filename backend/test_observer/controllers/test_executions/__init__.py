2d79a8e0b30587f04c82217c540d314e21646b90
Omar Selo <omar.selo@canonical.com>
2024-04-08 14:00:48 +0000

    Tag test executions endpoint to improve docs

--- backend/test_observer/controllers/test_executions/__init__.py
+++ backend/test_observer/controllers/test_executions/__init__.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from . import end_test, get_test_results, patch, start_test
 
-router = APIRouter()
+router = APIRouter(tags=["test-executions"])
 router.include_router(start_test.router)
 router.include_router(get_test_results.router)
 router.include_router(end_test.router)
