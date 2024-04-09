5215a84b5a46925b94129436032c2551ee6189fd
Omar Selo <omar.selo@canonical.com>
2024-04-09 09:39:54 +0000

    Add copyright notices

--- backend/tests/controllers/test_executions/test_get_test_results.py
+++ backend/tests/controllers/test_executions/test_get_test_results.py
@@ -14,10 +14,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-# Written by:
-#        Omar Selo <omar.selo@canonical.com>
-#        Nadzeya Hutsko <nadzeya.hutsko@canonical.com>
-
 
 from fastapi.testclient import TestClient
 
