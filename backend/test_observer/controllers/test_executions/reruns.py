e7a3fe231ee1ebed8f285bb0063319ea6972ad49
Omar Selo <omar.selo@canonical.com>
2024-04-30 07:34:19 +0000

    Rename restart endpoint to reruns to be more RESTFull

--- backend/test_observer/controllers/test_executions/reruns.py
+++ backend/test_observer/controllers/test_executions/reruns.py
@@ -9,18 +9,18 @@ from test_observer.data_access.setup import get_db
 router = APIRouter()
 
 
-class RestartRequest(BaseModel):
+class RerunRequest(BaseModel):
     test_execution_id: int
 
 
-@router.post("/restart")
-def create_a_restart_request(request: RestartRequest, db: Session = Depends(get_db)):
+@router.post("/reruns")
+def create_a_rerun_request(request: RerunRequest, db: Session = Depends(get_db)):
     te = db.get(TestExecution, request.test_execution_id)
     if not te:
         msg = f"No test execution with id {request.test_execution_id} found"
         raise HTTPException(status_code=404, detail=msg)
 
 
-@router.get("/restart", response_model=list[TestExecutionDTO])
-def get_test_executions_to_restart():
+@router.get("/reruns", response_model=list[TestExecutionDTO])
+def get_rerun_requests():
     return []
--- backend/tests/controllers/test_executions/test_reruns.py
+++ backend/tests/controllers/test_executions/test_reruns.py
@@ -2,16 +2,16 @@ from fastapi.testclient import TestClient
 
 from tests.data_generator import DataGenerator
 
-restart_url = "/v1/test-executions/restart"
+reruns_url = "/v1/test-executions/reruns"
 
 
 def test_post_no_data_returns_422(test_client: TestClient):
-    response = test_client.post(restart_url)
+    response = test_client.post(reruns_url)
     assert response.status_code == 422
 
 
 def test_post_invalid_id_returns_404_with_message(test_client: TestClient):
-    response = test_client.post(restart_url, json={"test_execution_id": 1})
+    response = test_client.post(reruns_url, json={"test_execution_id": 1})
     assert response.status_code == 404
     assert response.json()["detail"] == "No test execution with id 1 found"
 
@@ -22,11 +22,11 @@ def test_valid_post_returns_200(test_client: TestClient, generator: DataGenerato
     e = generator.gen_environment()
     te = generator.gen_test_execution(ab, e)
 
-    response = test_client.post(restart_url, json={"test_execution_id": te.id})
+    response = test_client.post(reruns_url, json={"test_execution_id": te.id})
     assert response.status_code == 200
 
 
 def test_get_returns_200_with_empty_list(test_client: TestClient):
-    response = test_client.get(restart_url)
+    response = test_client.get(reruns_url)
     assert response.status_code == 200
     assert response.json() == []
