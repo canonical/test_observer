497db31394bdf8c42aaf028ab31c950a35c021f7
Omar Selo <omar.selo@canonical.com>
2024-05-09 08:29:25 +0000

    fix seed script

--- backend/scripts/seed_data.py
+++ backend/scripts/seed_data.py
@@ -315,11 +315,11 @@ def seed_data(client: TestClient | requests.Session, session: Session | None = N
 def _rerun_some_test_executions(
     client: TestClient | requests.Session, test_executions: list[dict]
 ) -> None:
-    for te in test_executions[::2]:
-        client.post(
-            RERUN_TEST_EXECUTION_URL,
-            json={"test_execution_id": te["id"]},
-        ).raise_for_status()
+    te_ids = [te["id"] for te in test_executions[::2]]
+    client.post(
+        RERUN_TEST_EXECUTION_URL,
+        json={"test_execution_ids": te_ids},
+    ).raise_for_status()
 
 
 def _add_bugurl_and_duedate(session: Session) -> None:
