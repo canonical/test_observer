4588dc858e434cfd2483fb08b50281551a817b7c
Omar Selo <omar.selo@canonical.com>
2024-08-16 13:18:38 +0300

    Make tapping a header change url sort query parameters

--- frontend/lib/ui/dashboard/dashboard_body/artefacts_list_view/headers.dart
+++ frontend/lib/ui/dashboard/dashboard_body/artefacts_list_view/headers.dart
@@ -11,21 +11,42 @@ class _Headers extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final uri = AppRoutes.uriFromContext(context);
+
     return SizedBox(
       height: 56,
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: columnsMetaData
             .map(
               (data) => Expanded(
                 flex: data.flex,
-                child: Text(
-                  data.name,
-                  style: Theme.of(context).textTheme.titleLarge,
+                child: InkWell(
+                  onTap: () => _handleFilterTap(context, uri, data.name),
+                  child: Text(
+                    data.name,
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
                 ),
               ),
             )
             .toList(),
       ),
     );
+  }
+
+  void _handleFilterTap(BuildContext context, Uri pageUri, String columnName) {
+    final queryParameters = pageUri.queryParameters;
+    final sortBy = columnName;
+    final direction = queryParameters['sortBy'] == sortBy &&
+            queryParameters['direction'] == 'ASC'
+        ? 'DESC'
+        : 'ASC';
+    final newQueryParameters = {
+      ...queryParameters,
+      'sortBy': sortBy,
+      'direction': direction,
+    };
+    context.go(pageUri.replace(queryParameters: newQueryParameters).toString());
   }
 }
