b512e27a7e06f2ef0cdcb22d0e185e70f86f5a09
Omar Selo <omar.selo@canonical.com>
2024-08-19 14:16:26 +0300

    Fix sort query parameters vanishing when changing filters

--- frontend/lib/ui/page_filters/page_filters.dart
+++ frontend/lib/ui/page_filters/page_filters.dart
@@ -70,11 +70,17 @@ class PageFiltersView extends ConsumerWidget {
     Uri pageUri,
     BuildContext context,
   ) {
+    final sortBy = pageUri.queryParameters[CommonQueryParameters.sortBy];
+    final sortDirection =
+        pageUri.queryParameters[CommonQueryParameters.sortDirection];
     final searchValue = ref.read(searchValueProvider(searchQuery)).trim();
     final queryParams = {
       if (searchValue.isNotEmpty)
         CommonQueryParameters.searchQuery: searchValue,
       ...ref.read(pageFiltersProvider(pageUri)).toQueryParams(),
+      if (sortBy != null) CommonQueryParameters.sortBy: sortBy,
+      if (sortDirection != null)
+        CommonQueryParameters.sortDirection: sortDirection
     };
     context.go(
       pageUri.replace(queryParameters: queryParams).toString(),
