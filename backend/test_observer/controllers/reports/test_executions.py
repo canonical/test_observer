74a9b04ea9ca7eed4e8c5c6e9c8df38494c697c2
Andrej Velichkovski <andrej.velichkovski@gmail.com>
2024-12-05 10:25:58 +0100

    Move new endpoint to separate file

---
+++ backend/test_observer/controllers/reports/test_executions.py
@@ -0,0 +1,138 @@
+import csv
+from datetime import datetime
+from typing import Any
+
+from fastapi import APIRouter, Depends
+from fastapi.responses import FileResponse
+from sqlalchemy import select, func, Select
+from sqlalchemy.dialects.postgresql import aggregate_order_by
+from sqlalchemy.engine import Result
+from sqlalchemy.orm import Session
+from sqlalchemy.orm.attributes import InstrumentedAttribute
+
+from test_observer.data_access import queries
+from test_observer.data_access.models import (
+    Artefact,
+    ArtefactBuildEnvironmentReview,
+    Environment,
+    Family,
+    Stage,
+    TestCase,
+    TestEvent,
+    TestExecution,
+    TestResult,
+)
+from test_observer.data_access.setup import get_db
+
+
+router = APIRouter(tags=["reports2"])
+
+
+TEST_EXECUTIONS_REPORT_COLUMNS: list[Any] = [
+    Family.name,
+    Artefact.id,
+    Artefact.name,
+    Artefact.version,
+    Artefact.status,
+    Artefact.track,
+    Artefact.series,
+    Artefact.repo,
+    Artefact.created_at,
+    TestExecution.id,
+    TestExecution.status,
+    TestExecution.ci_link,
+    TestExecution.c3_link,
+    TestExecution.checkbox_version,
+    Environment.name,
+    Environment.architecture,
+    ArtefactBuildEnvironmentReview.review_decision,
+    ArtefactBuildEnvironmentReview.review_comment,
+]
+
+
+def _get_test_execution_reports_query(
+    columns: list[InstrumentedAttribute],
+    include_test_events: bool,
+    start_date: datetime,
+    end_date: datetime,
+) -> Select:
+    """
+    Builds the query that retrieves the test executions based on the parameters set
+    """
+    latest_builds = queries.latest_artefact_builds.subquery()
+    base_query = (
+        select(*columns)
+        .join_from(TestExecution, Environment)
+        .join_from(TestExecution, latest_builds)
+        .join_from(latest_builds, Artefact)
+        .join_from(Artefact, Stage)
+        .join_from(Stage, Family)
+        .join(
+            ArtefactBuildEnvironmentReview,
+            (ArtefactBuildEnvironmentReview.artefact_build_id == latest_builds.c.id)
+            & (ArtefactBuildEnvironmentReview.environment_id == Environment.id),
+        )
+        .where(
+            TestExecution.created_at >= start_date, TestExecution.created_at <= end_date
+        )
+    )
+
+    if include_test_events:
+        return base_query.outerjoin(
+            TestEvent, TestExecution.id == TestEvent.test_execution_id
+        ).group_by(*TEST_EXECUTIONS_REPORT_COLUMNS)
+
+    return base_query
+
+
+@router.get("/test-executions", response_class=FileResponse)
+def get_test_execution_reports(
+    start_date: datetime = datetime.min,
+    end_date: datetime | None = None,
+    include_test_events: bool = False,
+    db: Session = Depends(get_db),
+):
+    """
+    Returns a csv report detailing all test executions within a given date range.
+    Together with their artefact and environment details in csv format.
+    """
+
+    if end_date is None:
+        end_date = datetime.now()
+
+    columns = [*TEST_EXECUTIONS_REPORT_COLUMNS]
+    column_names = [str(column) for column in TEST_EXECUTIONS_REPORT_COLUMNS]
+    if include_test_events:
+        columns.extend(
+            [
+                func.array_agg(
+                    aggregate_order_by(TestEvent.event_name, TestEvent.timestamp)
+                ),
+                func.array_agg(
+                    aggregate_order_by(
+                        func.to_char(TestEvent.timestamp, "YYYY-MM-DD HH24:MI:SS:MS"),
+                        TestEvent.timestamp,
+                    ),
+                ),
+                func.array_agg(
+                    aggregate_order_by(TestEvent.detail, TestEvent.timestamp)
+                ),
+            ]
+        )
+        column_names.extend(
+            ["TestEvent.event_name", "TestEvent.timestamp", "TestEvent.detail"]
+        )
+
+    cursor = db.execute(
+        _get_test_execution_reports_query(
+            columns, include_test_events, start_date, end_date
+        )
+    )
+
+    filename = "test_executions_report.csv"
+    with open(filename, "w") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(column_names)
+        writer.writerows(cursor)
+
+    return filename
--- backend/test_observer/controllers/reports/test_results.py
+++ backend/test_observer/controllers/reports/test_results.py
@@ -4,21 +4,17 @@ from typing import Any
 
 from fastapi import APIRouter, Depends
 from fastapi.responses import FileResponse
-from sqlalchemy import select, func, Select
-from sqlalchemy.dialects.postgresql import aggregate_order_by
-from sqlalchemy.engine import Result
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 from sqlalchemy.orm.attributes import InstrumentedAttribute
 
 from test_observer.data_access import queries
 from test_observer.data_access.models import (
     Artefact,
-    ArtefactBuildEnvironmentReview,
     Environment,
     Family,
     Stage,
     TestCase,
-    TestEvent,
     TestExecution,
     TestResult,
 )
@@ -49,36 +45,6 @@ TESTRESULTS_REPORT_COLUMNS: list[InstrumentedAttribute] = [
     TestResult.created_at,
 ]
 
-TEST_EXECUTIONS_REPORT_COLUMNS: list[Any] = [
-    Family.name,
-    Artefact.id,
-    Artefact.name,
-    Artefact.version,
-    Artefact.status,
-    Artefact.track,
-    Artefact.series,
-    Artefact.repo,
-    Artefact.created_at,
-    TestExecution.id,
-    TestExecution.status,
-    TestExecution.ci_link,
-    TestExecution.c3_link,
-    TestExecution.checkbox_version,
-    Environment.name,
-    Environment.architecture,
-    ArtefactBuildEnvironmentReview.review_decision,
-    ArtefactBuildEnvironmentReview.review_comment,
-]
-
-
-def _report_csv_format(filename: str, header_row: list, cursor: Result) -> str:
-    with open(filename, "w") as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(header_row)
-        writer.writerows(cursor)
-
-    return filename
-
 
 @router.get("/test-results", response_class=FileResponse)
 def get_testresults_report(
@@ -107,89 +73,10 @@ def get_testresults_report(
         .where(Artefact.created_at >= start_date, Artefact.created_at <= end_date)
     )
 
-    return _report_csv_format(
-        "testresults_report.csv",
-        TESTRESULTS_REPORT_COLUMNS,
-        cursor,
-    )
+    filename = "testresults_report.csv"
+    with open(filename, "w") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(TESTRESULTS_REPORT_COLUMNS)
+        writer.writerows(cursor)
 
-
-def _get_test_execution_reports_query(
-    columns: list[InstrumentedAttribute],
-    include_test_events: bool,
-    start_date: datetime,
-    end_date: datetime,
-) -> Select:
-    """
-    Builds the query that retrieves the test executions based on the parameters set
-    """
-    latest_builds = queries.latest_artefact_builds.subquery()
-    base_query = (
-        select(*columns)
-        .join_from(TestExecution, Environment)
-        .join_from(TestExecution, latest_builds)
-        .join_from(latest_builds, Artefact)
-        .join_from(Artefact, Stage)
-        .join_from(Stage, Family)
-        .join(
-            ArtefactBuildEnvironmentReview,
-            (ArtefactBuildEnvironmentReview.artefact_build_id == latest_builds.c.id)
-            & (ArtefactBuildEnvironmentReview.environment_id == Environment.id),
-        )
-        .where(
-            TestExecution.created_at >= start_date, TestExecution.created_at <= end_date
-        )
-    )
-
-    if include_test_events:
-        return base_query.outerjoin(
-            TestEvent, TestExecution.id == TestEvent.test_execution_id
-        ).group_by(*TEST_EXECUTIONS_REPORT_COLUMNS)
-
-    return base_query
-
-
-@router.get("/test-executions", response_class=FileResponse)
-def get_test_execution_reports(
-    start_date: datetime = datetime.min,
-    end_date: datetime | None = None,
-    include_test_events: bool = False,
-    db: Session = Depends(get_db),
-):
-    """
-    Returns a csv report detailing all test executions within a given date range.
-    Together with their artefact and environment details in csv format.
-    """
-
-    if end_date is None:
-        end_date = datetime.now()
-
-    columns = [*TEST_EXECUTIONS_REPORT_COLUMNS]
-    column_names = [str(column) for column in TEST_EXECUTIONS_REPORT_COLUMNS]
-    if include_test_events:
-        columns.extend(
-            [
-                func.array_agg(
-                    aggregate_order_by(TestEvent.event_name, TestEvent.timestamp)
-                ),
-                func.array_agg(
-                    aggregate_order_by(
-                        func.to_char(TestEvent.timestamp, "YYYY-MM-DD HH24:MI:SS:MS"),
-                        TestEvent.timestamp,
-                    ),
-                ),
-                func.array_agg(
-                    aggregate_order_by(TestEvent.detail, TestEvent.timestamp)
-                ),
-            ]
-        )
-        column_names.extend(
-            ["TestEvent.event_name", "TestEvent.timestamp", "TestEvent.detail"]
-        )
-
-    cursor = db.execute(
-        _get_test_execution_reports_query(
-            columns, include_test_events, start_date, end_date
-        )
-    )
-    return _report_csv_format("test_executions_report.csv", column_names, cursor)
+    return filename
