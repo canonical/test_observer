dc6a072c69d5d8ee99f9aa14cac3fa472b518599
Omar Selo <omar.selo@canonical.com>
2025-01-06 09:50:49 +0000

    Insert optional launchpad_api paramter to add_user for testing

--- backend/test_observer/users/add_user.py
+++ backend/test_observer/users/add_user.py
@@ -6,10 +6,15 @@ from test_observer.external_apis.launchpad.launchpad_api import LaunchpadAPI
 from test_observer.external_apis.launchpad.models import LaunchpadUser
 
 
-def add_user(launchpad_email: str, session: Session | None = None):
-    launchpad_api = LaunchpadAPI()
-    launchpad_user = launchpad_api.get_user_by_email(launchpad_email)
+def add_user(
+    launchpad_email: str,
+    session: Session | None = None,
+    launchpad_api: LaunchpadAPI | None = None,
+):
+    if not launchpad_api:
+        launchpad_api = LaunchpadAPI()
 
+    launchpad_user = launchpad_api.get_user_by_email(launchpad_email)
     if not launchpad_user:
         raise ValueError("Email not registered in launchpad")
 
