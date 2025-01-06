a9efd6e9b79f0e706aa653a18379776aadd4618f
Omar Selo <omar.selo@canonical.com>
2025-01-06 10:02:47 +0000

    Replace real LaunchpadAPI with FakeLaunchpadAPI for test

--- backend/tests/users/test_add_user.py
+++ backend/tests/users/test_add_user.py
@@ -1,23 +1,26 @@
 import pytest
+from pydantic import EmailStr
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from scripts.add_user import add_user
 from test_observer.data_access.models import User
+from test_observer.external_apis.launchpad.launchpad_api import LaunchpadAPI
+from test_observer.external_apis.launchpad.models import LaunchpadUser
 
 
 def test_create_user(db_session: Session):
-    email = "omar.selo@canonical.com"
+    email = "john.doe@canonical.com"
 
-    user = add_user(email, db_session)
+    user = add_user(email, db_session, _FakeLaunchpadAPI())
 
     assert user
     assert (
         db_session.execute(
             select(User).where(
                 User.launchpad_email == email,
-                User.launchpad_handle == "omar-selo",
-                User.name == "Omar Abou Selo",
+                User.launchpad_handle == "john-doe",
+                User.name == "John Doe",
             )
         ).scalar_one_or_none()
         is not None
@@ -28,3 +31,12 @@ def test_email_not_in_launchpad():
     email = "john@doe.com"
     with pytest.raises(ValueError, match="Email not registered in launchpad"):
         add_user(email)
+
+
+class _FakeLaunchpadAPI(LaunchpadAPI):
+    def __init__(self):
+        # override superclass init
+        pass
+
+    def get_user_by_email(self, email: str) -> LaunchpadUser | None:
+        return LaunchpadUser(handle="john-doe", email=email, name="John Doe")
