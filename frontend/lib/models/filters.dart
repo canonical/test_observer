e15c9f0b158f6377529ea114c8eca0f1bf8f4f07
Omar Selo <omar.selo@canonical.com>
2025-01-17 11:22:44 +0300

    Add image specific filters

--- frontend/lib/models/filters.dart
+++ frontend/lib/models/filters.dart
@@ -84,11 +84,55 @@ class Filters<T> with _$Filters<T> {
 }
 
 Filters<Artefact> createEmptyArtefactFilters(FamilyName family) {
-  return emptyArtefactFilters;
+  switch (family) {
+    case FamilyName.image:
+      return emptyImageFilters;
+    default:
+      return emptyArtefactFilters;
+  }
 }
 
 final emptyArtefactFilters = Filters<Artefact>(
   filters: [
+    Filter<Artefact>(
+      name: 'Assignee',
+      extractOption: (artefact) => artefact.assignee?.name,
+    ),
+    Filter<Artefact>(
+      name: 'Status',
+      extractOption: (artefact) => artefact.status.name,
+    ),
+    Filter<Artefact>(
+      name: 'Due date',
+      extractOption: (artefact) {
+        final now = DateTime.now();
+        final dueDate = artefact.dueDate;
+
+        if (dueDate == null) return 'No due date';
+        if (dueDate.isBefore(now)) return 'Overdue';
+
+        final daysDueIn = now.difference(dueDate).inDays;
+        if (daysDueIn >= 7) return 'More than a week';
+        return 'Within a week';
+      },
+    ),
+  ],
+);
+
+final emptyImageFilters = Filters<Artefact>(
+  filters: [
+    Filter<Artefact>(
+      name: 'OS type',
+      extractOption: (artefact) => artefact.os,
+    ),
+    Filter<Artefact>(
+      name: 'Release',
+      extractOption: (artefact) => artefact.release,
+    ),
+    Filter<Artefact>(
+      name: 'Owner',
+      extractOption: (artefact) => artefact.owner,
+    ),
     Filter<Artefact>(
       name: 'Assignee',
       extractOption: (artefact) => artefact.assignee?.name,
