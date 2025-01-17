058ed492a7bf146bf3675a56f453eb57a8a9a092
Omar Selo <omar.selo@canonical.com>
2025-01-17 14:32:37 +0000

    Add copyright and license notices to backend files

--- backend/migrations/versions/2024_01_10_1231-c9851b127edc_add_artefact_signoff_assignees_and_user_.py
+++ backend/migrations/versions/2024_01_10_1231-c9851b127edc_add_artefact_signoff_assignees_and_user_.py
@@ -1,3 +1,17 @@
+# Copyright (C) 2023 Canonical Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 """Add artefact signoff assignees and user model
 
 Revision ID: c9851b127edc
--- backend/test_observer/common/constants.py
+++ backend/test_observer/common/constants.py
@@ -1,3 +1,17 @@
+# Copyright (C) 2023 Canonical Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 PREVIOUS_TEST_RESULT_COUNT = 10
 
 VALID_ISSUE_HOSTS = {"github.com", "warthogs.atlassian.net", "bugs.launchpad.net"}
