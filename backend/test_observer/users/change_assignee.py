0978dc0883902ab590ea259db7d0c88e2219b109
Omar Selo <omar.selo@canonical.com>
2025-01-17 14:32:37 +0000

    Add copyright and license notices to backend files

--- backend/test_observer/users/change_assignee.py
+++ backend/test_observer/users/change_assignee.py
@@ -1,3 +1,17 @@
+# Copyright (C) 2023 Canonical Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 from sqlalchemy.orm import Session
 
 from test_observer.data_access.models import Artefact, User
