6c8b88b693dc933d6a7e2626eeda826002901eaa
Matias Piipari <matias.piipari@gmail.com>
2025-02-04 11:17:25 +0200

    Format the schema with jq to avoid ginormous diffs on any change, and to improve readability

--- backend/scripts/fetch_openapi_schema.sh
+++ backend/scripts/fetch_openapi_schema.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -e
 poetry install >/dev/null 2>&1
 
 echo "Starting a transient copy of test-observer-api to fetch the OpenAPI schema..."
@@ -8,7 +9,7 @@ poetry run uvicorn test_observer.main:app --host 0.0.0.0 --port 30000 &
 
 for i in {1..5}; do
     if curl --output /dev/null --silent --head --fail "http://localhost:30000/openapi.json"; then
-        curl --silent http://localhost:30000/openapi.json -o schemata/openapi.json
+        curl --silent http://localhost:30000/openapi.json | jq > schemata/openapi.json
         git add schemata/openapi.json
         echo "OpenAPI schema fetched."
         success=true
