670c67300e0558416640bbc619c6039bbe6a9321
Omar Selo <omar.selo@canonical.com>
2025-02-12 16:52:46 +0300

    Remove unused import

--- frontend/lib/ui/artefact_page/environment_review_button.dart
+++ frontend/lib/ui/artefact_page/environment_review_button.dart
@@ -15,7 +15,6 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import 'package:flutter/material.dart';
-import 'package:yaru/yaru.dart';
 import 'package:popover/popover.dart';
 
 import '../../models/environment_review.dart';
