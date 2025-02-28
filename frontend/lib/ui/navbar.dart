2d4ad0b3734bf29f823b805260e2b747ec73da09
Omar Selo <omar.selo@canonical.com>
2025-02-28 13:41:45 +0300

    Fixed UI bug

--- frontend/lib/ui/navbar.dart
+++ frontend/lib/ui/navbar.dart
@@ -30,61 +30,63 @@ class Navbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      alignment: Alignment.center,
       color: YaruColors.coolGrey,
-      constraints: BoxConstraints.loose(
-        const Size.fromWidth(Spacing.maxPageContentWidth),
-      ),
-      padding: const EdgeInsets.symmetric(
-        horizontal: Spacing.pageHorizontalPadding,
-      ),
+      alignment: Alignment.center,
       height: _navbarHeight,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image.asset('assets/canonical.png'),
-          const SizedBox(width: Spacing.level4),
-          Expanded(
-            child: Row(
-              children: [
-                const _NavbarEntry(
-                  title: 'Snap Testing',
-                  route: AppRoutes.snaps,
-                ),
-                const _NavbarEntry(
-                  title: 'Deb Testing',
-                  route: AppRoutes.debs,
-                ),
-                const _NavbarEntry(
-                  title: 'Charm Testing',
-                  route: AppRoutes.charms,
-                ),
-                const _NavbarEntry(
-                  title: 'Image Testing',
-                  route: AppRoutes.images,
-                ),
-                const Spacer(),
-                _NavbarDropdownEntry(
-                  label: 'Help',
-                  dropdownChildren: [
-                    _NavbarDropdownItem(
-                      label: 'Docs',
-                      onPressed: () => launchUrlString(
-                        'https://canonical-test-observer.readthedocs-hosted.com/en/latest/',
+      child: Container(
+        constraints: BoxConstraints.loose(
+          const Size.fromWidth(Spacing.maxPageContentWidth),
+        ),
+        padding: const EdgeInsets.symmetric(
+          horizontal: Spacing.pageHorizontalPadding,
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset('assets/canonical.png'),
+            const SizedBox(width: Spacing.level4),
+            Expanded(
+              child: Row(
+                children: [
+                  const _NavbarEntry(
+                    title: 'Snap Testing',
+                    route: AppRoutes.snaps,
+                  ),
+                  const _NavbarEntry(
+                    title: 'Deb Testing',
+                    route: AppRoutes.debs,
+                  ),
+                  const _NavbarEntry(
+                    title: 'Charm Testing',
+                    route: AppRoutes.charms,
+                  ),
+                  const _NavbarEntry(
+                    title: 'Image Testing',
+                    route: AppRoutes.images,
+                  ),
+                  const Spacer(),
+                  _NavbarDropdownEntry(
+                    label: 'Help',
+                    dropdownChildren: [
+                      _NavbarDropdownItem(
+                        label: 'Docs',
+                        onPressed: () => launchUrlString(
+                          'https://canonical-test-observer.readthedocs-hosted.com/en/latest/',
+                        ),
                       ),
-                    ),
-                    _NavbarDropdownItem(
-                      label: 'Source Code',
-                      onPressed: () => launchUrlString(
-                        'https://github.com/canonical/test_observer',
+                      _NavbarDropdownItem(
+                        label: 'Source Code',
+                        onPressed: () => launchUrlString(
+                          'https://github.com/canonical/test_observer',
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
