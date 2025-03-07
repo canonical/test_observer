f3465853a6c1a3b400e7445277d3a75625ccc22c
Omar Selo <omar.selo@canonical.com>
2025-03-07 12:49:16 +0300

    Add MultiSelectFilter a more flexible alternative to Filter

---
+++ frontend/lib/filtering/multi_select_filter.dart
@@ -0,0 +1,11 @@
+class MultiSelectFilter<T> {
+  const MultiSelectFilter({
+    required this.name,
+    required this.filter,
+    required this.extractOptions,
+  });
+
+  final String name;
+  final List<T> Function(List<T> items, Set<String> options) filter;
+  final Set<String> Function(List<T> items) extractOptions;
+}
