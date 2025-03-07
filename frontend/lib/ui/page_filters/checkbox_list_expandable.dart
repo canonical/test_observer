60b32fffc73d273a8ca2937173317b1b6fa11a93
Omar Selo <omar.selo@canonical.com>
2025-03-07 13:08:57 +0300

    Add CheckboxListExpandable widget

---
+++ frontend/lib/ui/page_filters/checkbox_list_expandable.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:yaru/yaru.dart';
+
+import '../expandable.dart';
+
+class CheckboxListExpandable extends StatelessWidget {
+  const CheckboxListExpandable({
+    super.key,
+    required this.title,
+    required this.options,
+    required this.onChanged,
+  });
+
+  final String title;
+  final List<({String name, bool isSelected})> options;
+  final Function(String, bool) onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expandable(
+      initiallyExpanded: true,
+      title: Text(
+        title,
+        style: Theme.of(context).textTheme.headlineSmall,
+      ),
+      children: [
+        for (final option in options)
+          Row(
+            children: [
+              YaruCheckbox(
+                value: option.isSelected,
+                onChanged: (newValue) {
+                  if (newValue != null) {
+                    onChanged(option.name, newValue);
+                  }
+                },
+              ),
+              Text(option.name),
+            ],
+          ),
+      ],
+    );
+  }
+}
