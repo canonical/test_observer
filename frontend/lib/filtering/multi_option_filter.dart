7a720082143599728cd47962db7848579f402fae
Omar Selo <omar.selo@canonical.com>
2025-03-10 11:33:24 +0300

    Add MultiOptionFilter factory method

--- frontend/lib/filtering/multi_option_filter.dart
+++ frontend/lib/filtering/multi_option_filter.dart
@@ -1,3 +1,5 @@
+import 'package:dartx/dartx.dart';
+
 class MultiOptionFilter<T> {
   const MultiOptionFilter({
     required this.name,
@@ -9,3 +11,15 @@ class MultiOptionFilter<T> {
   final List<T> Function(List<T> items, Set<String> options) filter;
   final Set<String> Function(List<T> items) extractOptions;
 }
+
+MultiOptionFilter<T> createMultiOptionFilterFromExtractor<T>(
+  String name,
+  String Function(T) extractOption,
+) =>
+    MultiOptionFilter(
+      name: name,
+      extractOptions: (items) => items.map(extractOption).toSet(),
+      filter: (items, options) => items
+          .filter((item) => options.contains(extractOption(item)))
+          .toList(),
+    );
