69a2ea44985f580f8c939a4a32628649e66a7aa6
Omar Selo <omar.selo@canonical.com>
2025-03-10 13:50:49 +0300

    Filter test executions by environment name

--- frontend/lib/providers/filtered_enriched_test_executions.dart
+++ frontend/lib/providers/filtered_enriched_test_executions.dart
@@ -1,3 +1,4 @@
+import 'package:dartx/dartx.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../filtering/enriched_test_execution_filters.dart';
@@ -13,16 +14,27 @@ Future<List<EnrichedTestExecution>> filteredEnrichedTestExecutions(
   Uri pageUri,
 ) async {
   final artefactId = AppRoutes.artefactIdFromUri(pageUri);
+  final searchValue =
+      pageUri.queryParameters[CommonQueryParameters.searchQuery] ?? '';
+  final parameters = pageUri.queryParametersAll;
+
   List<EnrichedTestExecution> result =
       await ref.watch(enrichedTestExecutionsProvider(artefactId).future);
 
-  final parameters = pageUri.queryParametersAll;
   for (var filter in enrichedTestExecutionFilters) {
     final filterOptions = parameters[filter.name];
     if (filterOptions != null) {
       result = filter.filter(result, filterOptions.toSet());
     }
   }
+
+  result = result
+      .filter(
+        (ee) => ee.testExecution.environment.name
+            .toLowerCase()
+            .contains(searchValue.toLowerCase()),
+      )
+      .toList();
 
   return result;
 }
