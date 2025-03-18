5fd988c669d4ea38e5967acf961b73ebdef9a0f3
Omar Abou Selo <omar.selo@canonical.com>
2025-03-18 13:23:53 +0300

    Add test plan filters and rerun filtered test plans button (#278)
    * Add enriched test execution model to help with filtering
    
    * Add enriched test executions provider
    
    * Add MultiSelectFilter a more flexible alternative to Filter
    
    * Add testPlanNameFilter on EnrichedTestExecutions
    
    * Add filteredEnrichedTestExecutions provider
    
    * Rename MultiSelectFilter to MultiOptionFilter
    
    * Add CheckboxListExpandable widget
    
    * Small improvement
    
    * Add FiltersState provider
    
    * Add ArtefactPageSideFilters that uses FiltersState provider
    
    * Define FilterOptionState type
    
    * Migrate PageFilters to use FiltersStateProvider and CheckboxListExpandable
    
    * Add MultiOptionFilter factory method
    
    * Migrate Artefact filters to new format
    
    * Support artefact filters in filters state provider
    
    * Fix typing issue
    
    * Make FiltersState synchronous to fix filters vanishing on apply
    
    * Reduce CheckboxListExpandable padding
    
    * Rename FiltersState to PageFilters to ease reviewing and fix tests
    
    * Connect FilteredArtefactEnvironments with FilteredEnrichedTestExecutions
    
    * Filter test executions by environment name
    
    * Add default unknown test plan name to filter
    
    * Filter rename
    
    * Add environment review status filter
    
    * Add filter by status of last run of test plan
    
    * Sort filter options
    
    * Rename MultiOptionFilter to Filter
    
    * Fix artefacts filtering
    
    * Remove filter test plan name filter as it doesn't provide a nice UX
    
    * Fix test case
    
    * Add license notices
    
    * Rename to shorten name
    
    * Add rerun filtered plans button (#280)
    
    * Add rerun filtered plans button
    
    * Add missing copyright notice
    
    * Drop last execution status icons (#281)
    
    * Drop last execution status summary per environment
    
    * Drop last execution status icon next to environment expandable
    
    * Add test plan name filter (#282)
    
    * Bring back last status icon next to each environment
    
    * Add back environment status summary with reordering of statuses

---
+++ frontend/lib/filtering/enriched_test_execution_filters.dart
@@ -0,0 +1,75 @@
+// Copyright (C) 2023 Canonical Ltd.
+//
+// This file is part of Test Observer Frontend.
+//
+// Test Observer Frontend is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 3, as
+// published by the Free Software Foundation.
+//
+// Test Observer Frontend is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import 'package:dartx/dartx.dart';
+
+import '../models/enriched_test_execution.dart';
+import '../models/environment_review.dart';
+import '../models/test_execution.dart';
+import 'filter.dart';
+
+final environmentReviewFilter = createFilterFromExtractor(
+  'Review status',
+  (EnrichedTestExecution ee) => switch (ee.environmentReview.reviewDecision) {
+    [] => 'Undecided',
+    [EnvironmentReviewDecision.rejected] => 'Rejected',
+    [...] => 'Approved',
+  },
+);
+
+final testPlanLastStatusFilter = Filter<EnrichedTestExecution>(
+  name: 'Plans whose last run',
+  extractOptions: (items) {
+    final groups = items
+        .groupBy((ee) => (ee.environmentReview.id, ee.testExecution.testPlan));
+    final options = <String>{};
+    for (var enrichedExecutions in groups.values) {
+      final lastExecution =
+          enrichedExecutions.maxBy((ee) => ee.testExecution.id);
+      if (lastExecution != null) {
+        options.add(lastExecution.testExecution.status.name);
+      }
+    }
+    return options;
+  },
+  filter: (items, options) {
+    final groups = items
+        .groupBy((ee) => (ee.environmentReview.id, ee.testExecution.testPlan));
+    final filteredGroups = groups.entries.filter((entry) {
+      final lastStatus =
+          entry.value.maxBy((ee) => ee.testExecution.id)?.testExecution.status;
+      return lastStatus != null && options.contains(lastStatus.name);
+    });
+    return [
+      for (var group in filteredGroups)
+        for (var ee in group.value) ee,
+    ];
+  },
+);
+
+final testPlanNameFilter = createFilterFromExtractor<EnrichedTestExecution>(
+  'Test plan',
+  (ee) {
+    final testPlan = ee.testExecution.testPlan;
+    return testPlan.isEmpty ? TestExecution.defaultTestPlanName : testPlan;
+  },
+);
+
+final enrichedTestExecutionFilters = [
+  environmentReviewFilter,
+  testPlanLastStatusFilter,
+  testPlanNameFilter,
+];
