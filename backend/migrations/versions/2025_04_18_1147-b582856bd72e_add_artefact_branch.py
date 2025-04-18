660392ee5af858e4fb17d98c1ee439061d56ca35
Omar Abou Selo <omar.selo@canonical.com>
2025-04-18 15:04:02 +0300

    Add branch to Artefact model and create a db migration

---
+++ backend/migrations/versions/2025_04_18_1147-b582856bd72e_add_artefact_branch.py
@@ -0,0 +1,75 @@
+"""Add artefact branch
+
+Revision ID: b582856bd72e
+Revises: 6a726050c944
+Create Date: 2025-04-18 11:47:29.933275+00:00
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "b582856bd72e"
+down_revision = "6a726050c944"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    _add_branch_column()
+    _change_unique_constraints()
+
+
+def downgrade() -> None:
+    op.execute("DELETE FROM artefact WHERE branch != ''")
+    _revert_unique_constraints()
+    op.drop_column("artefact", "branch")
+
+
+def _add_branch_column():
+    op.add_column("artefact", sa.Column("branch", sa.String(length=200)))
+    op.execute("UPDATE artefact SET branch = '' WHERE branch is NULL")
+    op.alter_column("artefact", "branch", nullable=False)
+
+
+def _change_unique_constraints():
+    op.drop_index("unique_snap", "artefact")
+    op.drop_index("unique_charm", "artefact")
+
+    op.create_index(
+        "unique_snap",
+        "artefact",
+        ["name", "version", "track", "branch"],
+        unique=True,
+        postgresql_where=sa.text("family = 'snap'"),
+    )
+    op.create_index(
+        "unique_charm",
+        "artefact",
+        ["name", "version", "track", "branch"],
+        unique=True,
+        postgresql_where=sa.text("family = 'charm'"),
+    )
+
+
+def _revert_unique_constraints():
+    op.drop_index("unique_snap", "artefact")
+    op.drop_index("unique_charm", "artefact")
+
+    op.create_index(
+        "unique_snap",
+        "artefact",
+        ["name", "version", "track"],
+        unique=True,
+        postgresql_where=sa.text("family = 'snap'"),
+    )
+    op.create_index(
+        "unique_charm",
+        "artefact",
+        ["name", "version", "track"],
+        unique=True,
+        postgresql_where=sa.text("family = 'charm'"),
+    )
+    # ### end Alembic commands ###
--- backend/test_observer/data_access/models.py
+++ backend/test_observer/data_access/models.py
@@ -123,11 +123,16 @@ class Artefact(Base):
     status: Mapped[ArtefactStatus] = mapped_column(default=ArtefactStatus.UNDECIDED)
     archived: Mapped[bool] = mapped_column(Boolean, default=False, index=True)
 
-    # Family specific fields
+    # Snap and Charm specific fields
     track: Mapped[str] = mapped_column(default="")
     store: Mapped[str] = mapped_column(default="")
+    branch: Mapped[str] = mapped_column(String(200), default="")
+
+    # Deb specific fields
     series: Mapped[str] = mapped_column(default="")
     repo: Mapped[str] = mapped_column(default="")
+
+    # Image specific fields
     os: Mapped[str] = mapped_column(String(200), default="")
     release: Mapped[str] = mapped_column(String(200), default="")
     sha256: Mapped[str] = mapped_column(String(200), default="")
@@ -153,6 +158,7 @@ class Artefact(Base):
             "name",
             "version",
             "track",
+            "branch",
             postgresql_where=column("family") == FamilyName.snap.name,
             unique=True,
         ),
@@ -161,6 +167,7 @@ class Artefact(Base):
             "name",
             "version",
             "track",
+            "branch",
             postgresql_where=column("family") == FamilyName.charm.name,
             unique=True,
         ),
@@ -506,9 +513,9 @@ class ArtefactBuildEnvironmentReview(Base):
     __tablename__ = "artefact_build_environment_review"
     __table_args__ = (UniqueConstraint("artefact_build_id", "environment_id"),)
 
-    review_decision: Mapped[
-        list[ArtefactBuildEnvironmentReviewDecision]
-    ] = mapped_column(ARRAY(Enum(ArtefactBuildEnvironmentReviewDecision)), default=[])
+    review_decision: Mapped[list[ArtefactBuildEnvironmentReviewDecision]] = (
+        mapped_column(ARRAY(Enum(ArtefactBuildEnvironmentReviewDecision)), default=[])
+    )
     review_comment: Mapped[str] = mapped_column(default="")
 
     environment_id: Mapped[int] = mapped_column(
