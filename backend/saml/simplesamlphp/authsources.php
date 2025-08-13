6a7a2b363f9ae3df7bead2bd3514b36bd2f4acdd
Omar Abou Selo <omar.selo@canonical.com>
2025-08-13 12:01:22 +0300

    Configure SimpleSamlPHP to return user data similar to U1

---
+++ backend/saml/simplesamlphp/authsources.php
@@ -0,0 +1,18 @@
+<?php
+
+$config = array(
+
+    'admin' => array(
+        'core:AdminPassword',
+    ),
+
+    'example-userpass' => array(
+        'exampleauth:UserPass',
+        'omar-selo:password' => array(
+            'fullname' => 'Omar Abou Selo',
+            'lp_teams' => array('canonical-hw-cert'),
+            'email' => 'omar.selo@canonical.com'
+        ),
+    ),
+
+);
