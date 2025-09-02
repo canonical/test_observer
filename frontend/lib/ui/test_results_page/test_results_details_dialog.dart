e9f511751695849c28eef35f7f9e048a4b2a6ef3
Ahmet Uyar <ahmet.uyar@canonical.com>
2025-09-02 13:19:28 +0300

    Add copyright

--- frontend/lib/ui/test_results_page/test_results_details_dialog.dart
+++ frontend/lib/ui/test_results_page/test_results_details_dialog.dart
@@ -1,4 +1,18 @@
-// frontend/lib/ui/test_results_page/test_results_details_dialog.dart
+// Copyright (C) 2023 Canonical Ltd.
+//
+// This file is part of Test Observer Frontend.
+//
+// Test Observer Frontend is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 3, as
+// published by the Free Software Foundation.
+//
+// Test Observer Frontend is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import 'package:flutter/material.dart';
 import 'package:yaru/yaru.dart';
