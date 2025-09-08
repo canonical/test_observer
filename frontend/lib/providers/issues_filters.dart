7d6b966efad0612006dc132eed07ac49746309f1
Ryan Britton <ryan.britton@canonical.com>
2025-09-08 20:20:31 +0000

    Add issues page

---
+++ frontend/lib/providers/issues_filters.dart
@@ -0,0 +1,85 @@
+// Copyright (C) 2023 Canonical Ltd.
+//
+// This file is part of Test Observer Frontend.
+//
+// Test Observer Frontend is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 3, as
+// published by the Free Software Foundation.
+//
+// Test Observer Frontend is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../models/issue.dart';
+import 'issues.dart';
+
+part 'issues_filters.freezed.dart';
+part 'issues_filters.g.dart';
+
+@freezed
+abstract class IssuesFiltersState with _$IssuesFiltersState {
+  const factory IssuesFiltersState({
+    @Default({}) Set<IssueSource> possibleSources,
+    @Default({}) Set<String> possibleProjects,
+    @Default({}) Set<IssueStatus> possibleStatuses,
+    @Default({}) Set<IssueSource> selectedSources,
+    @Default({}) Set<String> selectedProjects,
+    @Default({}) Set<IssueStatus> selectedStatuses,
+  }) = _IssuesFiltersState;
+}
+
+@riverpod
+class IssuesFilters extends _$IssuesFilters {
+  static const sourceParam = 'source';
+  static const projectParam = 'project';
+  static const statusParam = 'status';
+
+  @override
+  IssuesFiltersState build(Uri pageUri) {
+    final issues = ref.watch(issuesProvider).value ?? [];
+    final params = pageUri.queryParametersAll;
+
+    // Sources
+    final selectedSources = IssueSource.values
+        .where((v) => (params[sourceParam] ?? []).contains(v.name))
+        .toSet();
+    final possibleSources = issues.map((i) => i.source).toSet();
+    final validSelectedSources = selectedSources.intersection(possibleSources);
+
+    // Projects
+    final selectedProjects = (params[projectParam] ?? []).toSet();
+    var filtered = issues;
+    if (selectedSources.isNotEmpty) {
+      filtered = filtered
+          .where((i) => validSelectedSources.contains(i.source))
+          .toList();
+    }
+    final possibleProjects = filtered.map((i) => i.project).toSet();
+    final validSelectedProjects =
+        selectedProjects.intersection(possibleProjects);
+
+    // Statuses
+    final possibleStatuses = IssueStatus.values.toSet();
+    final selectedStatuses = IssueStatus.values
+        .where((v) => (params[statusParam] ?? []).contains(v.name))
+        .toSet();
+    final validSelectedStatuses =
+        selectedStatuses.intersection(possibleStatuses);
+
+    return IssuesFiltersState(
+      possibleSources: possibleSources,
+      possibleProjects: possibleProjects,
+      possibleStatuses: possibleStatuses,
+      selectedSources: validSelectedSources,
+      selectedProjects: validSelectedProjects,
+      selectedStatuses: validSelectedStatuses,
+    );
+  }
+}
---
+++ frontend/lib/ui/issues_page/issues_page_side_visibility.dart
@@ -0,0 +1,31 @@
+// Copyright (C) 2023 Canonical Ltd.
+//
+// This file is part of Test Observer Frontend.
+//
+// Test Observer Frontend is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 3, as
+// published by the Free Software Foundation.
+//
+// Test Observer Frontend is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'issues_page_side_visibility.g.dart';
+
+@riverpod
+class IssuesPageSideVisibility extends _$IssuesPageSideVisibility {
+  @override
+  bool build() {
+    return true;
+  }
+
+  void set(bool value) {
+    state = value;
+  }
+}
