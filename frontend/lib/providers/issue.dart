cb64314582314afc38bd2ac706390659b974da83
Ahmet Uyar <85699024+uyarahmet@users.noreply.github.com>
2025-09-16 18:24:08 +0300

    [TO-146] Create issue page (#383)
    * Create issue page
    
    * Enlarge status
    
    * Replace issue string with issue source
    
    * Use IssueSourceWidget

---
+++ frontend/lib/providers/issue.dart
@@ -0,0 +1,28 @@
+// Copyright (C) 2023 Canonical Ltd.
+//
+// This file is part of Test Observer Frontend.
+//
+// Test Observer Frontend is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 3, as
+// published by the Free Software Foundation.
+//
+// Test Observer Frontend is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import '../models/issue.dart';
+import 'api.dart';
+
+part 'issue.g.dart';
+
+@riverpod
+Future<Issue> issue(Ref ref, int issueId) async {
+  final api = ref.watch(apiProvider);
+  return await api.getIssue(issueId);
+}
