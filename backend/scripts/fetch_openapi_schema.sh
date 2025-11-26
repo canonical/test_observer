4f4d6ab4f5c2a5c7c0e05836345f0d46f6c23856
Raul Almeida <raul.almeida@canonical.com>
2025-11-26 16:47:56 -0300

    enh: remove multiple retries

--- backend/scripts/fetch_openapi_schema.sh
+++ backend/scripts/fetch_openapi_schema.sh
@@ -6,21 +6,11 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Go to backend directory (parent of scripts)
 cd "$SCRIPT_DIR/.."
 
-for i in {1..5}; do
-    tmpfile=$(mktemp)
-    if curl --silent --fail "http://localhost:30000/openapi.json" -o "$tmpfile"; then
-        jq < "$tmpfile" > schemata/openapi.json
-        echo "OpenAPI schema fetched and written to schemata/openapi.json"
-        success=true
-        rm "$tmpfile"
-        break
-    else
-        rm "$tmpfile"
-        sleep 1
-    fi
-done
-
-if [ -z "$success" ]; then
-    echo "Failed to fetch openapi.json after multiple attempts."
-    exit 1
+tmpfile=$(mktemp)
+if curl --silent --fail "http://localhost:30000/openapi.json" -o "$tmpfile"; then
+    jq < "$tmpfile" > schemata/openapi.json
+    echo "OpenAPI schema fetched and written to schemata/openapi.json"
+else
+    echo "Failed to fetch openapi.json"
 fi
+rm "$tmpfile"
