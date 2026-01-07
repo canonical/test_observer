d5966fc883e2b6950d4925be302670e7c3f5485a
Ahmet Uyar <ahmet.uyar@canonical.com>
2026-01-07 16:04:13 +0300

    format

--- backend/test_observer/external_apis/jira/jira_client.py
+++ backend/test_observer/external_apis/jira/jira_client.py
@@ -98,7 +98,10 @@ class JiraClient:
         url = f"{self.base_url}/rest/api/{self.api_version}/issue/{issue_key}"
 
         params = {
-            "fields": "summary,status,resolution,resolutiondate,updated,created,issuetype,project",
+            "fields": (
+                "summary,status,resolution,resolutiondate,updated,"
+                "created,issuetype,project"
+            ),
         }
 
         try:
