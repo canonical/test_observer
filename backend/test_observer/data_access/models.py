a3350ec8959907115848057e837d6edabc8ff1c8
PeishuoCai <peishuo.cai@mail.utoronto.ca>
2026-02-20 17:12:01 -0500

    echo ":wq" | cd /home/peishuo.cai@canonical.com/test_observer && EDITOR=true git cherry-pick --continue  GIT_EDITOR=true git cherry-pick --continue  git status simplify auto-rerun logic to only cover future test results

--- backend/test_observer/data_access/models.py
+++ backend/test_observer/data_access/models.py
@@ -713,6 +713,7 @@ class Issue(Base):
     title: Mapped[str] = mapped_column(default="")
     status: Mapped[IssueStatus] = mapped_column(default=IssueStatus.UNKNOWN)
     last_synced_at = Column(DateTime, nullable=True)
+    auto_rerun_enabled: Mapped[bool] = mapped_column(default=False)
 
     test_result_attachments: Mapped[list["IssueTestResultAttachment"]] = relationship(
         back_populates="issue", cascade="all, delete"
