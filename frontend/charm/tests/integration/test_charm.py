08796ce79706ff8d35731030f7a05baaa1ecc7cf
Will Taylor <william.taylor@canonical.com>
2026-02-25 11:24:09 +0000

    Standardize copyright

--- frontend/charm/tests/integration/test_charm.py
+++ frontend/charm/tests/integration/test_charm.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 # Copyright 2023 Canonical Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +13,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+#
+# SPDX-FileCopyrightText: Copyright 2023 Canonical Ltd.
+# SPDX-License-Identifier: Apache-2.0
 
 import asyncio
 import logging
