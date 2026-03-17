ae9a492ecf8c94d84fc5a833ce260f6e287c7047
Raul Almeida <36135994+almeidaraul@users.noreply.github.com>
2026-03-17 15:54:56 -0300

    TO-51: show environment reviewers in frontend (#600)
    * feat: turn reviewer of artefact into list of reviewers (support multiple)
    
    * wip: test fixing
    
    * chore: update change_assignee keeping backwards compatibility
    
    * fix: failing tests; replace assignee with reviewer everywhere
    
    * tests: add multiple reviewers to artefacts
    
    * feat: use reviewer list in frontend (for now using only the first reviewer from the list)
    
    * chore: update API schema
    
    * feat: multiple reviewers for artefacts in frontend
    
    * tests: single-env artefact gets assigned a single reviewer
    
    * tests: artefact with many envs is assigned more than one reviewer (failing because feature is not implemented yet)
    
    * feat: number of assigned reviewers is a function of the number of environments for an artefact
    
    * feat: add reviewer list to artefact build environment review
    
    * tests: get and patch env reviews includes reviewers
    
    * feat: environment review API supports reviewers
    
    * feat: show environment reviewers in frontend
    
    * fix: syntax error
    
    * format: ruff format and check
    
    * chore: add licensing
    
    * fix: duplicate definition
    
    * enh: checking for null

--- frontend/lib/ui/artefact_page/environment_expandable.dart
+++ frontend/lib/ui/artefact_page/environment_expandable.dart
@@ -24,6 +24,7 @@ import '../expandable.dart';
 import '../spacing.dart';
 import 'environment_issues/environment_issues_expandable.dart';
 import 'environment_review_button.dart';
+import 'environment_reviewers_avatars.dart';
 import 'test_plan_expandable.dart';
 
 class EnvironmentExpandable extends ConsumerWidget {
@@ -110,6 +111,10 @@ class _EnvironmentExpandableTitle extends ConsumerWidget {
           style: Theme.of(context).textTheme.titleLarge,
         ),
         const Spacer(),
+        EnvironmentReviewersAvatars(
+          reviewers: artefactEnvironment.review.reviewers,
+        ),
+        const SizedBox(width: Spacing.level3),
         EnvironmentReviewButton(environmentReview: artefactEnvironment.review),
       ],
     );
