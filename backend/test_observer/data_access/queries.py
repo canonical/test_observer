c194fcb08ae25fda74f4ee048db28a48b9ac5ffa
Raul Almeida <raul.almeida@canonical.com>
2026-03-23 11:44:36 -0300

    enh: move artefact matching of rules to data_access

--- backend/test_observer/data_access/queries.py
+++ backend/test_observer/data_access/queries.py
@@ -13,9 +13,10 @@
 # SPDX-FileCopyrightText: Copyright 2024 Canonical Ltd.
 # SPDX-License-Identifier: AGPL-3.0-only
 
-from sqlalchemy import select
+from sqlalchemy import and_, or_, select
+from sqlalchemy.orm import Session
 
-from test_observer.data_access.models import ArtefactBuild
+from test_observer.data_access.models import Artefact, ArtefactBuild, ArtefactMatchingRule
 
 latest_artefact_builds = (
     select(ArtefactBuild)
@@ -26,3 +27,32 @@ latest_artefact_builds = (
         ArtefactBuild.revision.desc(),
     )
 )
+
+
+def match_artefact(artefact: Artefact, db: Session) -> list[ArtefactMatchingRule]:
+    family_str = artefact.family.value
+
+    possible_rules = (
+        db.execute(
+            select(ArtefactMatchingRule).where(
+                and_(
+                    ArtefactMatchingRule.family == family_str,
+                    or_(ArtefactMatchingRule.stage == artefact.stage, ArtefactMatchingRule.stage == ""),
+                    or_(ArtefactMatchingRule.track == artefact.track, ArtefactMatchingRule.track == ""),
+                    or_(ArtefactMatchingRule.branch == artefact.branch, ArtefactMatchingRule.branch == ""),
+                ),
+            )
+        )
+        .scalars()
+        .all()
+    )
+
+    # sort rules by number of non-empty fields to prioritize specificity
+    rules_with_score = [
+        [r, sum(1 for field in [r.stage, r.track, r.branch] if field != "")] for r in possible_rules
+    ]
+    sorted_rules = sorted(rules_with_score, key=lambda x: x[1], reverse=True)
+    highest_score = sorted_rules[0][1] if sorted_rules else 0
+    rules = [r[0] for r in sorted_rules if r[1] == highest_score]
+
+    return rules
