b022d895529c5cf8a8305ea7ddd1a9aff5049043
Raul Almeida <raul.almeida@canonical.com>
2026-03-31 12:45:15 -0300

    fix: formatting + license in latest migration

--- backend/migrations/versions/2026_03_31_1410-f0847700d32e_change_permissions_attributes_from_.py
+++ backend/migrations/versions/2026_03_31_1410-f0847700d32e_change_permissions_attributes_from_.py
@@ -1,3 +1,18 @@
+# Copyright 2026 Canonical Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License version 3, as
+# published by the Free Software Foundation.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# SPDX-FileCopyrightText: Copyright 2026 Canonical Ltd.
+# SPDX-License-Identifier: AGPL-3.0-only
+
 """Change permissions attributes from string to enum
 
 Revision ID: f0847700d32e
@@ -5,36 +20,44 @@ Revises: c29b4f545a9b
 Create Date: 2026-03-31 14:10:46.747275+00:00
 
 """
-from alembic import op
+
 import sqlalchemy as sa
+from alembic import op
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
-revision = 'f0847700d32e'
-down_revision = 'c29b4f545a9b'
+revision = "f0847700d32e"
+down_revision = "c29b4f545a9b"
 branch_labels = None
 depends_on = None
 
 PERMISSION_TYPE = postgresql.ARRAY(postgresql.ENUM(name="permission"))
 
+
 def upgrade() -> None:
     # upgrade application table
-    op.alter_column('application', 'permissions',
-               existing_type=postgresql.ARRAY(sa.VARCHAR()),
-               type_=PERMISSION_TYPE,
-               existing_nullable=False,
-               postgresql_using="permissions::permission[]")
+    op.alter_column(
+        "application",
+        "permissions",
+        existing_type=postgresql.ARRAY(sa.VARCHAR()),
+        type_=PERMISSION_TYPE,
+        existing_nullable=False,
+        postgresql_using="permissions::permission[]",
+    )
 
     # upgrade team table
     # drop default to prevent casting errors from postgresql
     op.execute("ALTER TABLE team ALTER COLUMN permissions DROP DEFAULT")
 
     # change type
-    op.alter_column('team', 'permissions',
-               existing_type=postgresql.ARRAY(sa.VARCHAR()),
-               type_=PERMISSION_TYPE,
-               existing_nullable=False,
-               postgresql_using="permissions::permission[]")
+    op.alter_column(
+        "team",
+        "permissions",
+        existing_type=postgresql.ARRAY(sa.VARCHAR()),
+        type_=PERMISSION_TYPE,
+        existing_nullable=False,
+        postgresql_using="permissions::permission[]",
+    )
 
     # apply new default
     op.execute("ALTER TABLE team ALTER COLUMN permissions SET DEFAULT '{}'::permission[]")
@@ -46,18 +69,24 @@ def downgrade() -> None:
     op.execute("ALTER TABLE team ALTER COLUMN permissions DROP DEFAULT")
 
     # convert
-    op.alter_column('team', 'permissions',
-               existing_type=PERMISSION_TYPE,
-               type_=postgresql.ARRAY(sa.VARCHAR()),
-               existing_nullable=False,
-               postgresql_using="permissions::varchar[]")
+    op.alter_column(
+        "team",
+        "permissions",
+        existing_type=PERMISSION_TYPE,
+        type_=postgresql.ARRAY(sa.VARCHAR()),
+        existing_nullable=False,
+        postgresql_using="permissions::varchar[]",
+    )
 
     # restore previous default
     op.execute("ALTER TABLE team ALTER COLUMN permissions SET DEFAULT '{}'::character varying[]")
 
     # upgrade application table
-    op.alter_column('application', 'permissions',
-               existing_type=PERMISSION_TYPE,
-               type_=postgresql.ARRAY(sa.VARCHAR()),
-               existing_nullable=False,
-               postgresql_using="permissions::varchar[]")
+    op.alter_column(
+        "application",
+        "permissions",
+        existing_type=PERMISSION_TYPE,
+        type_=postgresql.ARRAY(sa.VARCHAR()),
+        existing_nullable=False,
+        postgresql_using="permissions::varchar[]",
+    )
