520802426b8b26b65fc42d5c7e8d4c9c628b743e
Will Taylor <william.taylor@canonical.com>
2026-04-06 10:29:36 +0100

    Add permissions-related tests

--- backend/tests/controllers/applications/test_applications.py
+++ backend/tests/controllers/applications/test_applications.py
@@ -13,7 +13,9 @@
 # SPDX-FileCopyrightText: Copyright 2025 Canonical Ltd.
 # SPDX-License-Identifier: AGPL-3.0-only
 
+import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy.exc import ProgrammingError
 from sqlalchemy.orm import Session
 
 from test_observer.common.enums import Permission
@@ -141,3 +143,34 @@ def test_clear_application_permissions(test_client: TestClient, generator: DataG
 
     assert response.status_code == 200
     assert response.json()["permissions"] == []
+
+
+def test_create_invalid_permissions_api(test_client: TestClient):
+    response = make_authenticated_request(
+        lambda: test_client.post(
+            "/v1/applications",
+            json={"name": "myscript", "permissions": ["invalid_permission"]},
+        ),
+        Permission.add_application,
+    )
+
+    assert response.status_code == 422
+
+
+def test_update_invalid_permissions_api(test_client: TestClient, generator: DataGenerator):
+    application = generator.gen_application()
+
+    response = make_authenticated_request(
+        lambda: test_client.patch(
+            f"/v1/applications/{application.id}",
+            json={"permissions": ["invalid_permission"]},
+        ),
+        Permission.change_application,
+    )
+
+    assert response.status_code == 422
+
+
+def test_create_invalid_permissions_orm(generator: DataGenerator):
+    with pytest.raises(ProgrammingError):
+        generator.gen_application(permissions=["invalid_permission"])
