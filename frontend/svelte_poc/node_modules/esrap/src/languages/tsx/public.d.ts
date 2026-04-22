793337a646c12545f2bb5e7c13f3e303e722c485
Paul Goins <paul.goins@canonical.com>
2026-04-22 10:24:50 -0700

    Oops; removing accidentally-committed node modules

--- frontend/svelte_poc/node_modules/esrap/src/languages/tsx/public.d.ts
+++
@@ -1,5 +0,0 @@
-import type { Visitors, BaseNode } from '../../types';
-import type { TSOptions, BaseComment, Comment } from '../types';
-export type { BaseComment, Comment };
-export type Node = BaseNode;
-export default function tsx(options?: TSOptions): Visitors<BaseNode>;
