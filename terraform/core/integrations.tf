943a8a0753798a7d6d0cab34662bb51f2bbafb40
amc94 <amjad.chami@canonical.com>
2026-05-04 07:18:58 -0400

    add integrations

--- terraform/core/integrations.tf
+++ terraform/core/integrations.tf
@@ -1,3 +1,71 @@
+##### DB-S3 Integrations #####
+resource "juju_integration" "db-backups-restore" {
+  count      = var.deploy_test_observer_frontend && var.enable_backups ? 1 : 0
+  model_uuid = data.juju_model.model.uuid
+
+  application {
+    name = juju_application.backup-db[0].name
+  }
+
+  application {
+    name = juju_application.s3-integrator[0].name
+  }
+}
+
+resource "juju_integration" "db-backups" {
+  count      = var.deploy_test_observer_frontend && var.enable_backups ? 1 : 0
+  model_uuid = data.juju_model.model.uuid
+
+  application {
+    name = juju_application.database[0].name
+  }
+
+  application {
+    name = juju_application.s3-integrator[0].name
+  }
+}
+
+##### API - DB Integration #####
+resource "juju_integration" "test-observer-api-database-access" {
+  model_uuid = data.juju_model.model.uuid
+
+  application {
+    name = juju_application.test-observer-api.name
+  }
+
+  application {
+    name = juju_application.database[0].name
+  }
+}
+
+
+##### API - Frontend Integration #####
+resource "juju_integration" "test-observer-frontend-to-rest-api-access" {
+  model_uuid = data.juju_model.model.uuid
+
+  application {
+    name = juju_application.test-observer-api.name
+  }
+
+  application {
+    name = juju_application.test-observer-frontend[0].name
+  }
+}
+
+##### Redis API Integration #####
+resource "juju_integration" "test-observer-redis-access" {
+  model_uuid = data.juju_model.model.uuid
+
+  application {
+    name = juju_application.test-observer-api.name
+  }
+
+  application {
+    name = juju_application.redis.name
+  }
+}
+
+##### OTEL Integrations #####
 resource "juju_integration" "otelcol_prod_cos" {
   for_each   = data.juju_offer.cos_offers
   model_uuid = data.juju_model.model.uuid
