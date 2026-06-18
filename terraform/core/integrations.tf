56b1b78ece40a2291a3a00dccdb579eea1a3c2c2
Will Taylor <william.taylor@canonical.com>
2026-06-18 10:04:07 +0100

    Add copyright headers

--- terraform/core/integrations.tf
+++ terraform/core/integrations.tf
@@ -1,4 +1,21 @@
-# Database and redis relations are required for the API/backend
+# Copyright 2026 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-FileCopyrightText: Copyright 2026 Canonical Ltd.
+# SPDX-License-Identifier: Apache-2.0
+
+# Database relation is required for the API/backend
 resource "juju_integration" "backend_database" {
   model_uuid = data.juju_model.model.uuid
 
@@ -14,6 +31,7 @@ resource "juju_integration" "backend_database" {
   }
 }
 
+# Redis relation is required for the API/backend
 resource "juju_integration" "backend_redis" {
   model_uuid = data.juju_model.model.uuid
 
