de4f124b7f97f968bfdb9e517e83d60c54c13d9c
Raul Almeida <raul.almeida@canonical.com>
2026-07-16 15:29:35 -0300

    fix: commited by accident config.py

--- backend/test_observer/common/config.py
+++ backend/test_observer/common/config.py
@@ -20,7 +20,7 @@ SENTRY_DSN = os.getenv("SENTRY_DSN")
 SAML_SP_BASE_URL = os.getenv("SAML_SP_BASE_URL", "http://localhost:30000")
 SAML_IDP_METADATA_URL = os.getenv(
     "SAML_IDP_METADATA_URL",
-    "http://localhost:8081/simplesaml/saml2/idp/metadata.php",
+    "http://localhost:8080/simplesaml/saml2/idp/metadata.php",
 )
 SAML_SP_X509_CERT = os.getenv("SAML_SP_X509_CERT", "")
 SAML_SP_KEY = os.getenv("SAML_SP_KEY", "")
