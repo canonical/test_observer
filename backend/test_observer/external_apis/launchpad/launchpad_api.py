e92ca09ef9824d52152f4fb7553f3e68973d1207
Will Taylor <william.taylor@canonical.com>
2026-09-01 15:39:18 +0100

    Add Launchpad login timeout; Add pysocks dependency

--- backend/test_observer/external_apis/launchpad/launchpad_api.py
+++ backend/test_observer/external_apis/launchpad/launchpad_api.py
@@ -21,7 +21,9 @@ from .models import LaunchpadUser
 
 class LaunchpadAPI:
     def __init__(self):
-        self.launchpad = Launchpad.login_anonymously("test-observer", "production", version="devel")
+        self.launchpad = Launchpad.login_anonymously(
+            "test-observer", "production", version="devel", timeout=30
+        )
 
     def get_user_by_email(self, email: EmailStr) -> LaunchpadUser | None:
         user = self.launchpad.people.getByEmail(email=email)
